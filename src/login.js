52fd855d0dbf22e202259d3d91931cf1c5ac886b
min <kbpch02@naver.com>
2024-05-16 15:42:01 +0900

    6th commit

--- src/login.js
+++ src/login.js
@@ -4,13 +4,16 @@ import './index.css'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const [member, setMember] = useState({
     memberId: '',
     password: '',
   });
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -18,13 +21,56 @@ const Login = () => {
       const response = await axios.post('/member/login', member);
       
       if (response.status === 200) {
+        const token = response.data;
+        setCookie('token', token, { path: '/' });
+        fetchNickname(token);
         alert('로그인 성공!');
+        navigate('/', {replace: true});
       } else {
         alert('로그인 실패');
       }
     } catch (error) {
       console.error('로그인 요청 중 오류 발생:', error);
       alert('로그인 요청 중 오류가 발생했습니다.');
+    }
+  };
+
+  const setCookie = (name, value, options = {}) => {
+    options = {
+      path: '/',
+      // 기본적으로 쿠키는 현재 도메인의 모든 경로에서 접근 가능하도록 설정
+      ...options
+    };
+  
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+  
+    let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+  
+    for (let optionKey in options) {
+      updatedCookie += '; ' + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += '=' + optionValue;
+      }
+    }
+  
+    document.cookie = updatedCookie;
+  };
+
+  const fetchNickname = async (token) => {
+    try {
+      const response = await axios.post('/member/nickname', token);
+  
+      if (response.status === 200) {
+        const nickname = response.data;
+        console.log('닉네임:', nickname);
+      } else {
+        console.error('닉네임 조회 실패:', response.data);
+      }
+    } catch (error) {
+      console.error('닉네임 조회 중 오류 발생:', error);
     }
   };
 
--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function NavbarElement() {
+
+  const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState(null); // 추가 정보를 저장할 상태
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+  
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, "$1"); // 토큰 가져오기
+        if (token) {
+          const response = await axios.post('/member/nickname', token);
+          setUserInfo(response.data); // 유저 정보 설정
+        }
+      } catch (error) {
+        console.error('토큰 유효성 검증 중 오류 발생:', error);
+      }
+    };
+    fetchUserInfo();
+  }, [navigate]);
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
@@ -12,10 +35,19 @@ function NavbarElement() {
             <strong>IoTeacher</strong>  
           </Navbar.Brand>
 
+        {userInfo || (
           <Nav className="collapse navbar-collapse justify-content-end">
             <Nav.Link href="../login">Login</Nav.Link>
             <Nav.Link href="../register">Reigster</Nav.Link>
           </Nav>
+        )}
+          
+        
+        {userInfo && (
+        <Nav className="collapse navbar-collapse justify-content-end">
+          <Nav.Link>{userInfo}</Nav.Link>
+        </Nav>
+        )}
         </Container>
       </Navbar>
       <br />
